b3da191ac72aca9c7c595bbf48f870b5054a80c1
michaelpadilla504-cmyk <michaelpadilla504@gmail.com>
2026-07-29 17:05:18 -0600

    git commit -m "Se agrego la conexion a la base de datos"

--- config/conexion.py
+++ config/conexion.py
@@ -3,3 +3,13 @@
 - Define las funciones para abrir, commit y cerrar conexiones de forma segura.
 - Maneja excepciones de conexión a la base de datos.
 """
+import pyodbc
+
+def conectar():
+    return pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        "SERVER=.\\SQLEXPRESS;"
+        "DATABASE=Biblioteca360;"
+        "UID=biblioteca;"
+        "PWD=123456;"
+    )
