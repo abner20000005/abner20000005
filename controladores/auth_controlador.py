53e36710563108507db43b744ae889ac0e50b9c9
abner20000005 <abnercantarerocastillo@gmail.com>
2026-07-29 21:31:08 -0600

    Abner: Trabajo en controlador

--- controladores/auth_controlador.py
+++ controladores/auth_controlador.py
@@ -2,3 +2,5 @@
 - Valida las credenciales de usuario ingresadas en la pantalla de Login contra SQL Server.
 - Obtiene el rol del usuario para restringir o permitir el acceso a módulos específicos.
 """
+
+print("Hola")
