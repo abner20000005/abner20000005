5d17ccc35744747fadfe625de84cdfa2b31e6c16
jl0086222-cmyk <jl0086222@gmail.com>
2026-07-29 22:09:04 -0600

    Estefany:prueba

--- controladores/prestamo_controlador.py
+++ controladores/prestamo_controlador.py
@@ -2,3 +2,4 @@
 - Registra nuevos préstamos verificando previamente la disponibilidad del ejemplar y el límite del lector.
 - Procesa devoluciones, actualizando el estado del ejemplar a disponible.
 """
+print("Hola")
