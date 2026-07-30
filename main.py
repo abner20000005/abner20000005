0e20adabac559374c0d70d55d9116fe4de1b3d9b
michaelpadilla504-cmyk <michaelpadilla504@gmail.com>
2026-07-30 17:01:09 -0600

    git commit -m "Se ha modificado el archivo main.py "

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-"""""
-- Inicializa la ventana principal de la aplicación.
-- Carga la pantalla de Inicio de Sesión (Login).
-- Coordina el flujo de navegación entre los distintos módulos.
-<<<<<<< HEAD
-"""""
-=======
-"""""
-edad = int(input("Ingrese su edad: "))
-print(edad)
+from vistas.libros_vista import ventana
 
-"Un mensaje"
->>>>>>> 1be82426038cc994e9a9eeba0669a4bde7a351e0
+ventana.mainloop()
