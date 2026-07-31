9360747f0b4797d16198e54d11b43c7a1712b1a5
abner20000005 <abnercantarerocastillo@gmail.com>
2026-07-30 21:52:24 -0600

    Abner: Añadir otros archivos

--- vistas/login_vista.py
+++ vistas/login_vista.py
@@ -2,7 +2,7 @@
 - Diseña el formulario con cajas de texto para usuario y contraseña.
 - Contiene el botón de 'Iniciar Sesión' que invoca la validación en auth_controlador.py.
 """
-
+#Hecho por Sary
 import tkinter as tk 
 
 ventana1 = tk.Tk()
